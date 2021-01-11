1d4b03770adc97cfe7f948f487888e6d9e1cbbd4
yensaki <mov.an.double@gmail.com>
2021-01-11 15:14:24 +0900

    Crew Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import Link from 'next/link'
 import Layout from '../components/Layout'
 
+const Crew = () => (
+  <li>
+    <div>
+      aaa
+    </div>
+  </li>
+)
+
 const IndexPage = () => (
   <Layout title="Home | Next.js + TypeScript Example">
     <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
+    <ul>
+      <Crew></Crew>
+    </ul>
   </Layout>
 )
 
