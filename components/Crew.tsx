244210d635e1fe4f5d9b1cd619c68f777a797ff2
yensaki <mov.an.double@gmail.com>
2021-01-11 19:48:48 +0900

    Component Design

--- components/Crew.tsx
+++ components/Crew.tsx
@@ -1,51 +1,45 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
-enum Mark {
-    Crewmate = 'crewmate',
-    Impostor = 'impostor',
-    Gray = 'gray'
-}
-
 type Props = {
   color: string,
   name: string,
-  mark: Mark
 }
 
-const Crew = ({color, name, mark}: Props) => (
-    <li>
-        <Container >
-            <Square color={color}></Square>
-            <p>{name}</p>
-            {color}
-            <p>
-                {mark}
-            </p>
-            <div>
-                <input type="range" list="range-list"/>
-                <datalist id="range-list">
-                    <option value="0" />
-                    <option value="25" />
-                    <option value="50" />
-                    <option value="75" />
-                    <option value="100" />
-                </datalist>
-            </div>
-        </Container>
-    </li>
+const Crew = ({color, name}: Props) => (
+    <Container >
+        <div>
+            <div>{name}</div>
+            <Square style={{"background-color": color}}></Square>
+        </div>
+
+        <div>
+            <RangeSlider type="range" list="range-list"/>
+            <datalist id="range-list">
+                <option value="0" />
+                <option value="25" />
+                <option value="50" />
+                <option value="75" />
+                <option value="100" />
+            </datalist>
+        </div>
+    </Container>
 )
 
-type SquareProps = {
-    color: string
-}
-const Container = styled.div`
+const Container = styled.li`
+    width: 320px;
+    border: 1px solid #cee2f0;
+    border-radius: 5px 5px;
+    margin: 10px;
 `
 
-const Square = styled.div<SquareProps>`
-    width: 100px;
-    height: 100px;
-    background-color: red;
+const Square = styled.div`
+    width: 30px;
+    height: 30px;
+`
+
+const RangeSlider = styled.input`
+    width: 100%;
 `
 
 export default Crew
